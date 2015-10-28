aa2b7b8d00099642f9bac8e9d1084f0dc164762b
Jenny <jennifer.nastola@hhu.de>
2015-10-28 13:26:41 +0100

    Programm klappt :)

--- fibonacci.cxx
+++ fibonacci.cxx
@@ -1,1 +1,34 @@
+/**
+ * 	Jennifer Nastola, 2078140
+ *	
+ * 	fibonacci.cxx
+ */
 
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	int N;
+
+	cout << "\nHallo, hier wird die N-te Fibonacci-Zahl berechnet. " << endl;
+	cout << "Gib bitte eine positive, ganze Zahl ein: ";
+	cin >> N;
+
+	int f0 = 0;
+	int f1 = 1;
+	int f;
+
+	for(int i = 2; i <= N; i++) {
+		f = f1 + f0;
+		f0 = f1;
+		f1 = f;
+	}
+
+	cout << "\nDas Ergebnis ist: f(" << N << ") = " << f << endl;
+
+
+	return 0;
+}
